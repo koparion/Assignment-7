5f507853e8227ccda5a39bf9dfe5ffd0042978a6
rus <kipnis.rus@gmail.com>
2022-01-11 15:28:30 -0500

    developed an error, return is not read

--- src/App.js
+++ src/App.js
@@ -3,24 +3,11 @@ import GiphySearch from './components/GifRegSearchField';
 
 function App() {
   return (
+    <div className="App">
     <GiphySearch/>
 
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+    
+     </div>
   );
 }
 
--- src/components/GifRegSearchField.js
+++ src/components/GifRegSearchField.js
@@ -1,16 +1,41 @@
-import react, {setState, useEffect} from 'react'
+import react, {Fragment, useEffect, useState} from 'react'
 import axios from 'axios'
+import GiphyCard from './GifCard';
 
 function GiphySearch (props) {
-    const [gif, setGif] =  setState({});
-
+    const [gif, setGif] =  useState({});
 
     const API = "rccQGZ4FcBfr08fUwUOQsbBod383I8Aa"
-    let RegularSearch = useEffect(() => {
-        axios.get("http://api.giphy.com/v1/gifs/search?q="+searchField+"&api_key="+API).then(response)
-        
+    RegularSearch = useEffect(() => 
+        {
+            axios.get("http://api.giphy.com/v1/gifs/search?q="+searchField+"&api_key="+API).then((response) => 
+            {
+                setGif(response.data)
+            }); 
+        },[]);
+    
+    //user input
+    const searchField  = (event) => {
+        event.preventDefault();
+        const body = {gif};
+        event.target.value;
     }
-    //api key rccQGZ4FcBfr08fUwUOQsbBod383I8Aa
-}
+
+    return 
+    (
+        <div>
+            <form onSubmit={searchField}>
+                <input
+                type="text"
+                value={gif}
+                onChange={(event) => setGif(event.target.value)}
+                ></input>
+                <button placeholder='Search Gif'>  
+                </button>
+            </form>
+        </div>
+    )
+
+}// end of function
 
 export default GiphySearch;
