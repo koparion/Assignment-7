1fe3f260d8ba76db908fa5ddc0f48db9cd588257
rus <kipnis.rus@gmail.com>
2022-01-13 20:06:15 -0500

    update

--- src/components/GifCard.js
+++ src/components/GifCard.js
@@ -1,59 +1,64 @@
 import react from "react";
 // import Search from './GifRegSearchField'
 
+
 function GiphyCard(props) {
-  const { random, search, list, randomsearch , searching, trending } = props;
+  const { random, search, list, randomsearch, searching, trending } = props;
 
   return (
     <>
-      {randomsearch && random.map((element) => {
-        return (
-          <div className="container">
-            <ul>
-              <li key={element.id} className="card-img">
-                <a href={element.url}>
-                  <img
-                    src={element.images.fixed_height_small.url}
-                    alt="gif"
-                  ></img>
-                </a>
-              </li>
-            </ul>
-          </div>
-        );
-      })}
-      {searching && search.map((element) => {
-        return (
-          <div className="container">
-            <ul>
-              <li key={element.id} className="card-img">
-                <a href={element.url}>
-                  <img
-                    src={element.images.fixed_height_small.url}
-                    alt="gif"
-                  ></img>
-                </a>
-              </li>
-            </ul>
-          </div>
-        );
-      })}
-      {trending && list.map((element) => {
-        return (
-          <div className="container">
-            <ul>
-              <li key={element.id} className="card-img">
-                <a href={element.url}>
-                  <img
-                    src={element.images.fixed_height_small.url}
-                    alt="gif"
-                  ></img>
-                </a>
-              </li>
-            </ul>
-          </div>
-        );
-      })}
+      {randomsearch &&
+        random.map((element) => {
+          return (
+            <div className="container">
+                <ul>
+                  <li key={element.id} className="card-img">
+                    <a href={element.url}>
+                      <img
+                        src={element.images.fixed_height_small.url}
+                        alt="gif"
+                      ></img>
+                    </a>
+                  </li>
+                </ul>
+                </div>
+          );
+        })}
+      {searching &&
+        search.map((element) => {
+          return (
+            <div className="container">
+              <ul>
+                <li key={element.id} className="card-img">
+                  <a href={element.url}>
+                    <img
+                      src={element.images.fixed_height_small.url}
+                      alt="gif"
+                    ></img>
+                  </a>
+                </li>
+              </ul>
+            </div>
+            
+          );
+        })}
+      {trending &&
+        list.map((element) => {
+          return (
+            <div className="container">  
+              <ul>
+                <li key={element.id} className="card-img">
+                  <a href={element.url}>
+                    <img
+                      src={element.images.fixed_height_small.url}
+                      alt="gif"
+                    ></img>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          );
+        })}
     </>
   );
 }
